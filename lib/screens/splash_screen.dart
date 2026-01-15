adb9239fc614cf418b7fa1c98f818f3b265ab1c9
Shaziya Ansari <shaziyaa04811@gmail.com>
2026-01-15 06:59:51 +0000

    first:commit

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dashboard_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const DashboardScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: const Center(
+        child: Text(
+          'Music Genre App 🎵',
+          style: TextStyle(color: Colors.white, fontSize: 26),
+        ),
+      ),
+    );
+  }
+}
